b17c2127b71425787467c83d90126c9917561c7a
rehyann saini <rehyannsaini@gmail.com>
2024-06-20 16:15:53 +0530

    Mark3

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
+
     <section className="max-w-lg mx-auto text-center">
       <h1 className="text-7xl font-bold">Fund your creative work</h1>
       <h2 className="mt-4 mb-8">Blehh bleh lbeh <br />
-      bleh vlheasdas</h2>
+        bleh vlheasdas</h2>
       <button className="bg-yellow-300 px-8 py-4 font-bold rounded-full">Start </button>
-
+    "Cecking line"
     </section>
   );
 }
